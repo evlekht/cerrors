5ff6f960d20f11ed8b9d8b326f7e5b5b6a500c68
evlekht <ev.lekht@gmail.com>
2020-09-26 06:59:01 +0300

    Add files via upload

---
+++ cerros.go
@@ -0,0 +1,37 @@
+package cerrors
+
+import "fmt"
+
+type CustomError struct {
+	errs      []error
+	message   string
+	delimiter string `default:", "`
+}
+
+func (cerr CustomError) Error() string {
+	n := len(cerr.errs)
+	if n == 0 {
+		return ""
+	}
+	message := cerr.errs[0].Error()
+	for i := 1; i < n; i++ {
+		message += cerr.delimiter + cerr.errs[i].Error()
+	}
+	return message
+}
+
+func (cerr CustomError) Append(err error) {
+	cerr.errs = append(cerr.errs, err)
+}
+
+func New() *CustomError {
+	return &CustomError{
+		errs: make([]error, 0),
+	}
+}
+
+func Newf(msg string, args ...interface{}) *CustomError {
+	cerr := New()
+	cerr.message = fmt.Sprintf(msg, args...)
+	return cerr
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/evlekht/cerrors
+
+go 1.12
